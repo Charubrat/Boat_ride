da17655ae36f9a5fdf1d51402b5328a3042b22a5
SwagatGogoi110 <SwagatGogoi110>
2023-09-17 19:46:40 +0530

    first commit

---
+++ src/main/java/com/springboot/boat_ride/BoatRideApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.boat_ride;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatRideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/boat_ride/BoatRideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.boat_ride;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
